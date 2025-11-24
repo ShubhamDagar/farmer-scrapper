f41d0a09003e36e59a3d930e72b97e8218b6acf0
shubhamdagar <shubhamdagarnsit@gmail.com>
2025-11-24 15:20:59 +0530

    CORS added

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(
     app,
-    resources={r"/*": {"origins": "http://localhost:3000"}},
+    resources={r"/*": {
+        "origins": [
+            "http://localhost:3000",
+            "https://farmerportal-2xfo.onrender.com"
+        ]
+    }},
     supports_credentials=True
 )
 
